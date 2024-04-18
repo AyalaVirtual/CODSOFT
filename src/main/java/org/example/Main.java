96be245b2b323fac67977bb28583637a919fca34
AyalaVirtual <erica.ayala.work@gmail.com>
2024-04-17 21:19:08 -0400

    Edit comments detailing different methods. Create instance of NumberGame in Main class and call mehods.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-
+        NumberGame numberGame = new NumberGame(0, 0);
+        numberGame.startGame();
     }
 }
--- src/main/java/org/example/NumberGame.java
+++ src/main/java/org/example/NumberGame.java
@@ -30,7 +30,7 @@ public class NumberGame {
     }
 
 
-    // Prompt the user to enter their guess for the generated number.
+    // Prompt the user to enter their guess for the generated number. Bonus: Limit the number of attempts the user has to guess the number. Compare the user's guess with the generated number and provide feedback on whether the guess is correct, too high, or too low. Repeat until the user guesses the correct number.
     public void getUserGuess(int randomNum, int guesses) {
         if (guesses >= 1) {
             System.out.println("Enter your guess.");
@@ -61,14 +61,6 @@ public class NumberGame {
         }
     }
 
-      /*
-        3. Compare the user's guess with the generated number and provide feedback on whether the guess
-        is correct, too high, or too low.
-        4. Repeat steps 2 and 3 until the user guesses the correct number.
+      // Add the option for multiple rounds, allowing the user to play again. Display the user's score, which can be based on the number of attempts taken or rounds won.
 
-        You can incorporate additional details as follows:
-        5. Limit the number of attempts the user has to guess the number.
-        6. Add the option for multiple rounds, allowing the user to play again.
-        7. Display the user's score, which can be based on the number of attempts taken or rounds won.
-     */
 }
